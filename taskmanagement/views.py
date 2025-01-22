e8d334ee75970720b6ccef1375e009ea5a8fa78f
scrumbler <irishmjn@gmail.com>
2025-01-22 07:43:42 +0545

    Updated the templates

--- taskmanagement/views.py
+++ taskmanagement/views.py
@@ -4,7 +4,23 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 
 def home(request): #logic that i need to show are written here
-    return render(request, 'home.html')
+    person = [
+    {
+        'name': "test",
+        "age": 20,
+        },
+    {
+        'name': "test",
+        "age": 20,
+        },
+    {
+        'name': "test",
+        "age": 20,
+        }
+    ]
+    context = {"name": "homepage",
+               "persons": person}
+    return render(request, 'home.html',context)
 
 def index(request): #logic that i need to show are written here
     return HttpResponse("This is index function")
